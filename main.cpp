22d221c0e4eb739f587466633df9a2ff70988577
MatteoFrascolla <117918697+MatteoFrascolla@users.noreply.github.com>
2022-11-24 19:25:45 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,5 +2,19 @@
 using namespace std;
 
 int main() {
+   int a=1, b=1, c=0, n=0;
+    cin >> n;
+    if (n>=2){
+        cout << a << endl;
+        cout << b << endl;
+        for (int i=0; i+2<n; i++){
+            c=a+b;
+            cout << c << endl;
+            a=b;
+            b=c;
+        }
+    }else{
+        cout << "errore" << endl;
+    }
    return 0;
 }
